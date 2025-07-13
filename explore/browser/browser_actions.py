efe79f26003fe573a70d199f86013d354f134394
Uday Krishna <usarma02@gmail.com>
2025-07-13 07:43:36 +0000

    WIP

--- explore/browser/browser_actions.py
+++ explore/browser/browser_actions.py
@@ -2,6 +2,7 @@ from browser import get_browser_page
 from oxylabs.proxy import get_proxy_given_country
 from helpers.forms import FormParser
 from helpers.llm_functions import is_search_form
+from helpers.products import ProductParser
 import asyncio
 import requests
 from bs4 import BeautifulSoup
@@ -88,7 +89,7 @@ class BrowserTask:
             print(f"Navigated to {url}")
             html = await self.page.content()
             print(f"HTML fetched for {url}")
-            f = open(f'{urlparse(url).netloc}.html', 'w')
+            f = open(f'{urlparse(url).netloc}.search_results.html', 'w')
             f.write(html)
             f.close()
             return html
@@ -145,6 +146,102 @@ class BrowserTask:
                 pass
             print(e)
             return None
+
+    async def get_products_from_html(self, url):
+        html = await self.get_html(url)
+        if not html:
+            return None
+        products_parser = ProductParser(html)
+        products = products_parser.parse_all_links_from_html(html)
+
+        from locale import currency
+        from urllib.parse import urlparse
+        from pydantic import BaseModel
+        from typing import List
+
+        class Product(BaseModel):
+            index: int
+            href: str
+            css_path: List[str]
+            path: str
+            max_common: str
+
+        def get_raw_link(href: str) -> str:
+            return urlparse(href).netloc + urlparse(href).path
+
+
+        current_url = url
+
+
+        def if_external_link(href: str, current_url: str) -> bool:
+            if href.startswith('http'):
+                return urlparse(href).netloc != urlparse(current_url).netloc
+            if href.startswith('/'):
+                return False
+            if urlparse('https://' + href).netloc != urlparse(current_url).netloc:
+                return True
+            return False
+
+
+        products_split: list[Product] = [
+            Product(index=idx, href=get_raw_link(p['href']), css_path=p['css_path'].split(' > '), path=p['css_path'], max_common='')
+            for idx,p in enumerate(products)
+            if p['href'] and not if_external_link(p['href'], current_url)
+        ]
+
+        print("products_split", len(products_split))
+
+        grouped = {}
+
+        for p in products_split:
+            if p.href not in grouped:
+                grouped[p.href] = []
+            grouped[p.href].append(p)
+
+        print("grouped", len(grouped))
+
+        def get_most_common_parent(v):
+            min_length = min([len(p.css_path) for p in v])
+
+            if len(set([p.css_path[0] for p in v])) > 1:
+                return None
+
+            for i in range(0, min_length):
+                if len(set([p.css_path[i] for p in v])) > 1:
+                    return Product(
+                        index=v[0].index,
+                        href=v[0].href,
+                        css_path=v[0].css_path[:i],
+                        path=' > '.join(v[0].css_path[:i]),
+                        max_common=''
+                    )
+            return None
+
+        candidates = []
+
+        for v in grouped.values():
+            if len(v) == 1:
+                candidates.append(v[0])
+            else:
+                parent = get_most_common_parent(v)
+                if parent:
+                    candidates.append(parent)
+                else:
+                    candidates += v
+
+        print("candidates", len(candidates))
+
+        for candidate in candidates:
+            await self.page.addStyleTag(content=f'''
+                {" > ".join(candidate.css_path)} {{
+                    border: 2px solid red !important;
+                }}
+            ''')
+            await self.page.waitFor(500)
+
+        print(products_split)
+        print(candidates)
+        return products
 
     async def load_page_and_execute(self, url, country):
         if not self.browser:
@@ -262,12 +359,13 @@ async def run_browser_tasks():
     task_browser = BrowserTask("in")
     
 
-    websites = ['https://www.moglix.com', 'https://www.reliancedigital.in', 'https://play.google.com', 'https://www.smartprix.com', 'https://www.swiggy.com', 'https://www.flipkart.com', 'https://blinkit.com', 'https://www.bigbasket.com', 'https://www.91mobiles.com', 'https://www.poorvika.com', 'https://www.zeptonow.com', 'https://dir.indiamart.com', 'https://www.ajio.com', 'https://www.myntra.com', 'https://www.vijaysales.com', 'https://m.snapdeal.com', 'https://www.croma.com', 'https://www.nykaafashion.com']
+    # websites = ['https://www.moglix.com', 'https://www.reliancedigital.in', 'https://play.google.com', 'https://www.smartprix.com', 'https://www.swiggy.com', 'https://www.flipkart.com', 'https://blinkit.com', 'https://www.bigbasket.com', 'https://www.91mobiles.com', 'https://www.poorvika.com', 'https://www.zeptonow.com', 'https://dir.indiamart.com', 'https://www.ajio.com', 'https://www.myntra.com', 'https://www.vijaysales.com', 'https://m.snapdeal.com', 'https://www.croma.com', 'https://www.nykaafashion.com']
     # websites = ['https://www.jiomart.com']
-    
+
+    websites = ['https://www.flipkart.com/search?q=iphone%2016%20max%20pro']
     # Sequential execution
     for url in websites:
-        await task_browser.load_page_and_execute(url, "in")
+        await task_browser.get_products_from_html(url)
     
     await task_browser.cleanup()
 
